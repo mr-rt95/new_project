0d3e0aabe082b4655946b3933ad727c667678728
mr-rt95 <mr.rtahir95@gmail.com>
2025-05-19 08:25:21 +0400

    feat(footer): completed footer layout, added responsiveness and finalized the entire website

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,30 @@
-function openModal() {
-    alert("İletişim bilgileri açıldı.");
-    // Здесь может быть код, который показывает модальное окно, меню и т.д.
-}
+// function openModal() {
+//     alert("İletişim bilgileri açıldı.");
+// }
+
+// let scrollSpeed = 0;
+// let isScrolling = false;
+
+// window.addEventListener("wheel", function (e) {
+//     e.preventDefault();
+//     scrollSpeed += e.deltaY;
+
+//     if (!isScrolling) smoothScroll();
+// }, { passive: false });
+
+// function smoothScroll() {
+//     isScrolling = true;
+//     scrollSpeed *= 0.9;
+
+//     window.scrollBy(0, scrollSpeed);
+
+//     if (Math.abs(scrollSpeed) > 0.5) {
+//         requestAnimationFrame(smoothScroll);
+//     } else {
+//         scrollSpeed = 0;
+//         isScrolling = false;
+//     }
+// }
+
+
+Scrollbar.init(document.body);
